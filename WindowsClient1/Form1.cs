e7f7c7fb565ace7ea397d27a1eafa9a4993388a5
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2018-12-10 08:11:13 +0100

    fucked it up

--- WindowsClient1/Form1.cs
+++ WindowsClient1/Form1.cs
@@ -27,5 +27,20 @@ namespace WindowsClient1
 
             channel = ChannelFactory<IMathService>.CreateChannel(new BasicHttpBinding(), endPoint);
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MathServiceReference.MyNumbers obj = new MathServiceReference.MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = proxy.Add(obj).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MathServiceReference.MyNumbers obj = new MathServiceReference.MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = proxy.Subtract(obj).ToString();
+        }
     }
 }
--- WindowsClient2/Form1.cs
+++ WindowsClient2/Form1.cs
@@ -22,7 +22,18 @@ namespace WindowsClient2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            MathServiceReference.MyNumbers obj = new MathServiceReference.MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = proxy.Add(obj).ToString();
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MathServiceReference.MyNumbers obj = new MathServiceReference.MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = proxy.Subtract(obj).ToString();
         }
     }
 }
